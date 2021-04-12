d7e899a59e16fb7a66935617c33b47f738b9b3ab
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-12 18:29:18 +0530

    added :Welcome file

---
+++ main/java/com/datastructure/MyNode.java
@@ -0,0 +1,7 @@
+package com.datastructure;
+
+public class MyNode <K> {
+   public static void main(String[] args){
+       System.out.println("Welcome in data Structure");
+   }
+}
---
+++ test/java/com/MyNodeTest.java
@@ -0,0 +1,9 @@
+package com;
+
+import com.datastructure.MyNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyNodeTest {
+
+}
