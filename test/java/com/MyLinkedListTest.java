be2fb807209b91fbfef82bfae85486d6a18339cd
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-18 10:57:01 +0530

    added:node should be added in last

--- test/java/com/MyLinkedListTest.java
+++ test/java/com/MyLinkedListTest.java
@@ -23,6 +23,23 @@ public class MyLinkedListTest {
         System.out.println(result);
         Assertions.assertTrue(result);
     }
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList .head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        System.out.println(result);
+        Assertions.assertTrue(result);
+    }
+
 
 
 
