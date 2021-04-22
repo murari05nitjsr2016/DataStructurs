bca90f851be2154d4bac6366f61d1c9b189954bc
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-22 14:14:52 +0530

    added: add three nodes into stack

---
+++ test/java/com/MyStackTest.java
@@ -0,0 +1,29 @@
+package com;
+
+import com.datastructure.INode;
+import com.datastructure.MyNode;
+import com.datastructure.MyStack;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyStackTest {
+/*
+This test case checks that last added node in stack should be
+at top,because stack follows LIFO.
+ */
+    @Test
+    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode(){
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        myStack.printStack();
+        INode peak = myStack.peak();
+        Assertions.assertEquals(myThirdNode,peak);
+
+    }
+
+}
